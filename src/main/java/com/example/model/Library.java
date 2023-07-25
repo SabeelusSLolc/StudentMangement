609389686b24389de3f05d4af1fe777a98538de5
Sabeelus Saraf[COR/EKO/LOITS] <sabeeluss@lolctech.com>
2023-07-25 23:18:53 +0530

    Library entity

---
+++ src/main/java/com/example/model/Library.java
@@ -0,0 +1,92 @@
+package com.example.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Library {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String isbn;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String subtitle;
+    
+    @ElementCollection
+    private List<String> authors = new ArrayList<>();
+
+    @Column(nullable = false)
+    private String coverImageUrl;
+
+    public Library() {
+    }
+
+    public Library(String isbn, String title, String subtitle, List<String> authors, String coverImageUrl) {
+        this.isbn = isbn;
+        this.title = title;
+        this.subtitle = subtitle;
+        this.authors = authors;
+        this.coverImageUrl = coverImageUrl;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<String> authors) {
+        this.authors = authors;
+    }
+
+    public String getCoverImageUrl() {
+        return coverImageUrl;
+    }
+
+    public void setCoverImageUrl(String coverImageUrl) {
+        this.coverImageUrl = coverImageUrl;
+    }
+}
